eddd2a1fb88649d90b5b782499d97d02bfe15465
Murilo Rerison de Sousa Paz <murilokapaz@gmail.com>
2020-01-14 10:40:17 -0300

    code arranged

--- DLLCreateScheduleExcel/Services/TimelineRange.cs
+++ DLLCreateScheduleExcel/Services/TimelineRange.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using DLLCreateScheduleExcel.Entities;
-using DLLCreateScheduleExcel.Services.EnumsServices;
 using ClosedXML.Excel;
 using System.Globalization;
 
